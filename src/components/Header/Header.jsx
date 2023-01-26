f0fa0c086288df06bfa9e6f63ee614163d00c9b2
Bambotims <timilehinadebambo@gmail.com>
2023-01-26 22:58:27 +0100

    initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,14 +9,17 @@ const Header = () => {
     <header>
       <Navigation />
       <div className="header-main">
-        <div className="heading">Welcome to Wakanda</div>
+        <div className="heading">
+          bundled benefit for the gig economy
+          <CustomButton text="Get Benefit" />
+        </div>
         <div className="header-image">
           <img src={ManPic} alt="ManPic" />
         </div>
       </div>
       <div className="header-footer">
-        With only <span className="pink">30,000 Naira</span> you can get the
-        benefit from you need from a company
+        With as little as <span>30,000 Naira</span> you get the benefits of
+        working for a big company, even if you are not...-1
       </div>
     </header>
   );
