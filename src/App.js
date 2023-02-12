33eaa2ce4ac2fa2d324688d8a81a8d1a022fa15c
Sanni-Anibire Toyyib A <53148076+alabbaad@users.noreply.github.com>
2023-02-12 15:55:41 +0100

    copies

--- src/App.js
+++ src/App.js
@@ -34,9 +34,10 @@ const App = () => {
         <div className="section-text">
           <h2 className="purple">Get Benefit for Startup and Teams</h2>
           <p>
-            Getbenefit offers startups and teams looking for cost-effective
-            benefits a wide range of services to support the grouth and success
-            of their teams
+          As an employer you know how benefits helps retain top talents Whilenavigating the 
+          complex world of benefits can be a daunting task, Getbenefit empowers you to offer 
+          a comprehensive range of benefits to your employees, from health insurance to retirement
+          plans, simplifying the process and giving your team the edge they need to succeed.
           </p>
           <CustomButton text="Get Benefit" />
         </div>
@@ -47,8 +48,10 @@ const App = () => {
             Get Benefit for Remote Workers and Freelancers
           </h2>
           <p>
-            From flexible co-working spaces to low-cost Internet Subscriptions.
-            Getbenefit helps remote workers and tailored to their specific needs
+          Being a freelancer or remote worker doesn't mean you have to go without benefits. 
+          Getbenefit provides access to the benefits you need, whether that's health insurance,
+           retirement plans, or something else. With getbenefit, navigating the 
+           complex world of benefits is a breeze.
           </p>
           <CustomButton text="Get Benefit" />
         </div>
@@ -61,32 +64,38 @@ const App = () => {
         <Tags
           image={Broadband}
           heading="Broadband"
-          text="Helping employees and freelancers access cost-efficient data plans, allowing them stay connected and productive."
+          text="Helping employees and freelancers access cost-efficient data plans, allowing them stay 
+          connected and productive."
         />
         <Tags
           image={Health}
           heading="Health"
-          text="Partnering with HMOs and other healthcare providers to offer comprehensive healthcare coverage to employees."
+          text="Getting access to comprehensive and affordable healthcare coverage, to live healthy and 
+          work healthy."
         />
         <Tags
           image={Education}
           heading="Education"
-          text="Providing employees and freelancers with opportunities to further their education and skills development, helping them stay competitive in the workplace."
+          text="Providing employees and freelancers with opportunities and resources to further their education and 
+          skills development, helping them stay competitive in the workplace."
         />
         <Tags
           image={Wallet}
           heading="401(k)"
-          text="Helping employees secure their financial future by offering access to top pension managers and 401k plans."
+          text="Helping employees secure their financial future by offering access to top pension managers 
+          and 401k plans."
         />
         <Tags
           image={Chair}
           heading="Co-working Space"
-          text="Partnering with co-working spaces to offer employees and freelancers access to productive and collaborative workspaces."
+          text="Get access to productive, and collaborative co-working spaces wherever you find yourself. 
+          Just walk in and get your job done."
         />
         <Tags
           image={Vacation}
           heading="Vacation"
-          text="Providing employees and freelancers with opportunities to further their education and skills development, helping them stay competitive in the workplace."
+          text="Providing employees and freelancers with opportunities to further their education and skills 
+          development, helping them stay competitive in the workplace."
         />
       </Section>
       <Section className="no-flex">
@@ -95,7 +104,7 @@ const App = () => {
           <Info
             image={Illust1}
             heading="Onboard"
-            text="Sign up to get benefit and crate an account"
+            text="Sign up to getbenefit and create an account"
           />
           <Info
             image={Illust2}
@@ -105,12 +114,13 @@ const App = () => {
           <Info
             image={Illust3}
             heading="Get Advice"
-            text="You can chose to speak with a representative who will advice you based on what plans or bundles you can opt for."
+            text="Speak with a representative who will advice you based on what plans or bundles will be suitable 
+            for you."
           />
           <Info
             image={Illust4}
             heading="Get Benefits"
-            text="You make payment and start using your getbeneft voucher whenever you need to."
+            text="You make payment and Getbenefit whenever you need to."
           />
         </div>
       </Section>
@@ -119,8 +129,9 @@ const App = () => {
           <img src={Spend} alt="Thank" className="section-thank" />
         </div>
         <div className="thank">
-          "Spend more time being more productive, save more and get the most
-          benefits out of life."
+          "Whether you're an employer looking to offer competitive benefits or an independent worker 
+          looking to access them, Getbenefit simplifies the process, 
+          making it easy to find and manage the benefits you need to thrive."
         </div>
       </Section>
       <Section>
@@ -171,21 +182,33 @@ const App = () => {
         <div className="faq-faq">
           <Faq
             question="What Benefits does GetBenefit offers?"
-            answer="WELCOME"
+            answer="Getbenefit offers a wide range of benefits, 
+            including health, broadband plans, 401(k)/Pension, education, vacation and flexible co-working spaces."
+          />
+           <Faq
+            question=" Is Getbenefit only for startups and teams?"
+            answer="No, Getbenefit is also available for freelancers and remote workers. 
+            Our platform is tailored to the specific needs of startups, teams, and remote workers."
           />
           <Faq
             question="How much does it cost to use get benefits?"
-            answer="WELCOME"
+            answer="With as little as 30,000 naira, you can get access benefits you deserve."
           />
-          <Faq question="Can I Customize my benefits plans?" answer="WELCOME" />
-          <Faq question="What do I do when I am confused?" answer="WELCOME" />
+          <Faq question="Can I Customize my benefits plans?" 
+          answer="Yes, Getbenefit allows users to choose the benefits that they need,
+           making it easy for them to find the best fit for their specific needs." />
+          <Faq question="What do I do when I am confused?" 
+          answer="Yes, you can schedule a call with our team if you have any 
+          questions or need assistance with our platform." />
           <Faq
             question="Does GetBenefit offers 24/7 support?"
-            answer="WELCOME"
+            answer="Yes, Getbenefit team is always ready to assist, 
+            providing support and assistance when needed."
           />
           <Faq
             question="Is GetBenefit available in all locations?"
-            answer="WELCOME"
+            answer="Currently, Getbenefit is available in Nigeria, 
+           with plans to expand to other locations soon."
           />
         </div>
       </Section>
